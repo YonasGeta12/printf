b4587722efed78284f4f3429c3723b51bc9845fd
mavisoft <iyoboiyoha@gmail.com>
2022-10-18 19:16:18 -0700

    my third commit

---
+++ _write_char.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _write_char - writets the character c to stdout
+ * @c: The character to print
+ *
+ * Return On success 1. 
+ * On error, -1 is returned, and errorno is set appropriately.
+ */
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
